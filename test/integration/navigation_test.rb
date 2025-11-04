977502cc633822f43ff6a8baef697591d6f63af5
Hamed Montazeri <hamed.mon@gmail.com>
2025-11-04 10:10:54 +0100

    Add integration test for assessment layout rendering with theme stylesheet

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class NavigationTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    Rails::Assessment.registry.reset
+
+    Rails::Assessment.define "layout-check" do
+      title "Layout Check"
+      hook "Ensure engine layout renders."
+
+      question "Ready?" do
+        option "Yes", tag: :yes
+      end
+    end
+  end
+
+  teardown do
+    Rails::Assessment.registry.reset
+  end
+
+  test "assessment show page renders engine layout with theme stylesheet" do
+    get "/rails-assessment/layout-check"
+    assert_response :success
+
+    assert_select "body.rails-assessment-body"
+    assert_includes response.body, "rails/assessment/theme"
+  end
 end
